2ea24b16943a75af64506127d4ef013192c6eb3f
33kumarram <95571520+33kumarram@users.noreply.github.com>
2023-01-06 08:29:38 +0530

    login functionality

--- users/users.services.js
+++ users/users.services.js
@@ -1,4 +1,6 @@
 const db = require("../helpers/db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   CreateNewUser,
@@ -7,20 +9,26 @@ module.exports = {
 
 async function CreateNewUser(params) {
   let user = new db.Users(params);
+  const salt = await bcrypt.genSalt();
+  user.password = await bcrypt.hash(params.password, salt);
   await user.save();
   return user;
 }
 async function signIn(params) {
-  let user = await db.Users.find({ mobile_no: params.mobile_no });
-  console.log(user[0].password,params.password)
-  if (user&&user[0].password === params.password) {
-    console.log("i am running");
-    let refreshToken = jwt.sign({
-      sub: user.mobile_no,
-      mobile_no: user.mobile_no,
-    });
-    return refreshToken;
-  } else {
-    throw "Invalid password or mobile number";
+  let user = await db.Users.findOne({ mobile_no: params.mobile_no });
+  const isMatch= await bcrypt.compare(params.password,user.password)
+  if(!isMatch){
+    throw 'Incorrect password'
+  }
+
+  const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+  return {token,
+    user:{
+      id:user._id,
+      mobile_no:user.mobile_no,
+      displayName:user.displayName,
+      email_id:user.email_id
+    }
   }
 }
